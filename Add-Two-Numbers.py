5986aeda282b13a0ac6507f01a7fa2a7bab493f9
Kelsey Gaetjens <kjgaetjens@Kelseys-MacBook-Pro.local>
2019-06-25 13:59:39 -0500

    Add Two Numbers assignment - done

---
+++ Add-Two-Numbers.py
@@ -0,0 +1,9 @@
+#Create an app which takes two numbers as input from the user and then prints out the results of addition on the screen. 
+
+def add_two_nums(num1,num2):
+  print(num1+num2)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+add_two_nums(num1, num2)
