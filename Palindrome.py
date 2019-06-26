a6ac7a1fc7691bb6df6baab65f93b4be72f5620a
Kelsey Gaetjens <kjgaetjens@Kelseys-MacBook-Pro.local>
2019-06-26 15:55:20 -0500

    Refactored palindrome assignment. Previously turned string into explicit list before reversing. Cut out this step and used extended slicing of string instead.

--- Palindrome.py
+++ Palindrome.py
@@ -17,13 +17,12 @@ def input_string():
 
 #reverse the string
 def reverse_string(word):
-    letters = list(word.lower())
-    reversed_letters = [letters[index] for index in range(len(letters)-1, -1, -1)]
-    return (letters, reversed_letters)
+    reversed_string = word[::-1]
+    return reversed_string
 
 #determine if string equals reverse string (eg palindrome)
-def determine_is_palindrome(letters, reversed_letters):
-    if letters == reversed_letters:
+def determine_is_palindrome(word, reversed_word):
+    if word.lower() == reversed_word.lower():
         return True
     else:
         return False
@@ -37,8 +36,39 @@ def display_is_palindrome(is_palindrome):
     
 #execute functions
 word = input_string()
-letters, reversed_letters = reverse_string(word)
-is_palindrome = determine_is_palindrome(letters, reversed_letters)
+reversed_word = reverse_string(word)
+is_palindrome = determine_is_palindrome(word, reversed_word)
 display_is_palindrome(is_palindrome)
 
 
+#Deprecated code - figured out a way to reverse string without taking extra step to make it an explicit list
+# #prompt user to input string
+# def input_string():
+#     original_string = input("Enter a word: ")
+#     return original_string
+
+# #reverse the string
+# def reverse_string(word):
+#     letters = list(word.lower())
+#     reversed_letters = [letters[index] for index in range(len(letters)-1, -1, -1)]
+#     return (letters, reversed_letters)
+
+# #determine if string equals reverse string (eg palindrome)
+# def determine_is_palindrome(letters, reversed_letters):
+#     if letters == reversed_letters:
+#         return True
+#     else:
+#         return False
+
+# #print result to user
+# def display_is_palindrome(is_palindrome):
+#     if is_palindrome == True:
+#         print(f"{word} is a palindrome :) ")
+#     elif is_palindrome == False:
+#         print(f"{word} is not a palindrome :(")
+    
+# #execute functions
+# word = input_string()
+# letters, reversed_letters = reverse_string(word)
+# is_palindrome = determine_is_palindrome(letters, reversed_letters)
+# display_is_palindrome(is_palindrome)
