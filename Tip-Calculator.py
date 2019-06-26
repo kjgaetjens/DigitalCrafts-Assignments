b1a9e0b515441abb505221ca980ed90203caa42d
Kelsey Gaetjens <kjgaetjens@Kelseys-MacBook-Pro.local>
2019-06-26 09:23:11 -0500

    Added notes from review session

--- Tip-Calculator.py
+++ Tip-Calculator.py
@@ -13,3 +13,5 @@ total_amount = input("Enter the total amount: ")
 tip_percentage = input("Enter the tip percentage amount: ")
 
 tip_calc(total_amount, tip_percentage)
+
+#review-notes: consider breaking tip-calculator into smaller functions to make each code block easier to re-use and make code updates cleaner. eg make input one function, make display tip one function.
