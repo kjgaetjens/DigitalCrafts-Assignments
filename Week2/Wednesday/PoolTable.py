10773125bd40161ee1af2a90565ba4439c07bb79
Kelsey Gaetjens <kjgaetjens@Kelseys-MBP.verizon.net>
2019-07-05 16:04:32 -0500

    PoolTableManagement assignment - done (but could use some refactoring)

--- Week2/Wednesday/PoolTable.py
+++ Week2/Wednesday/PoolTable.py
@@ -1,17 +1,31 @@
 from datetime import datetime
 
 class PoolTable:
-    def __init__(self, number, is_occupied):
+    def __init__(self, number, is_occupied, start_time = datetime(1111,1,1,0,0,0,0), end_time = datetime(1111,1,1,0,0,0,0), total_time = 0):
         self.number = number
         self.is_occupied = is_occupied
-        self.start_time = datetime(1111,1,1,12)
-        self.end_time = datetime(1111,1,1,12)
-        self.total_time = 0 #end_time - start_time
+        self.start_time = start_time
+        self.end_time = end_time
+        self.total_time = total_time
 
-    # #move these somewhere else
-    # @staticmethod
-    # def time_spent(start_time, end_time):
-    #     pass
+    def to_dictionary(self):
+        return {
+            "number": self.number, 
+            "start_time": datetime.strftime(self.start_time, "%m/%d/%Y %H:%M"),
+            "end_time": datetime.strftime(self.end_time, "%m/%d/%Y %H:%M"),
+            "total_minutes_played": self.total_time,
+            "cost_dollar": self.total_time/60 * 30
+            }        
 
-    # @staticmethod
-    # def check_if_occupied
+    def to_dictionary_quit(self):
+        return {
+            "number": self.number,
+            "is_occupied": self.is_occupied,
+            "start_time": datetime.strftime(self.start_time, "%m/%d/%Y %H:%M:%S:%f"),
+            "end_time": datetime.strftime(self.end_time, "%m/%d/%Y %H:%M:%S:%f"),
+            "total_minutes_played": self.total_time,
+            }
+
+def from_dictionary_quit(dict):
+    return PoolTable(dict["number"], dict["is_occupied"], datetime.strptime(dict["start_time"], "%m/%d/%Y %H:%M:%S:%f"), datetime.strptime(dict["end_time"], "%m/%d/%Y %H:%M:%S:%f"), dict["total_minutes_played"])
+
--- Week2/Wednesday/PoolTableManagement.py
+++ Week2/Wednesday/PoolTableManagement.py
@@ -4,7 +4,7 @@
 # DONE - As an admin you should be able to see all the tables (12) 
 # DONE - As an admin each table in the list should show, whether the table is OCCUPIED or NOT OCCUPIED. 
 # DONE - As an admin if the table is OCCUPIED then show the start time of the table, number of minutes played. (Hardmode - If the minutes are > 60 then show them in terms of hours) 
-# - As an admin you can only give out the tables that are NOT OCCUPIED. This means if pool table 8 is occupied and you try to give it out then the app will print a message saying "Pool Table 8 is currently occupied". 
+# DONE - As an admin you can only give out the tables that are NOT OCCUPIED. This means if pool table 8 is occupied and you try to give it out then the app will print a message saying "Pool Table 8 is currently occupied". 
 # - As an admin whenever I close the table it should write an entry in the text file / json file. The file should be named in the following format: (11-22-2017.txt or 11-22-2017.json) keeping track of all the tables. The entry can consists of the following information: 
 # _________________________________________
 
@@ -19,44 +19,83 @@
 # MORE HARD MODE - Write Unit Tests for your application
 # EXTREMELY HARD MODE: Add the ability to email the final report (file) to an email address. 
 
-#do i want the file to keep track of current and use it to pull table info for view, etc?
-##maybe move all my functions into the class - like check in and check out
+
+
+## Need to move more of my functions to separate files where it makes sense.
 
 
 import json
 from datetime import datetime
 from PoolTable import PoolTable
+from PoolTable import from_dictionary_quit
+from os import path
 
+
+def create_json_log_if_needed():
+    if path.exists(current_date_path) == True:
+        pass
+    elif path.exists(current_date_path) == False:
+        with open(current_date_path,"w") as file_object:
+            json.dump([], file_object)
 
 def create_initial_tables(numberOfTables):
     for i in range(numberOfTables):
         pool_table = PoolTable(i+1,False)
         tables.append(pool_table)
 
+def quit_application():
+    table_statuses = []
+    for table in tables:
+        table_statuses.append(table.to_dictionary_quit())
+    with open("table_status.json","w") as file_object:
+           json.dump(table_statuses, file_object)
+    exit()
+
+def load_statuses():
+    if path.exists("table_status.json") == True:
+        with open("table_status.json") as file_object: 
+            table_dictionaries = json.load(file_object)
+        table_classes = [from_dictionary_quit(table) for table in table_dictionaries]
+        for table in table_classes:
+            tables.append(table)
+    elif path.exists("table_status.json") == False:
+        with open("table_status.json","w") as file_object:
+            json.dump([], file_object)
+        create_initial_tables(12) #move the parameter for this
+
 def menu():
     print("Main Menu")
     print("1) View Status of All Tables")
     print("2) Check-out a Table")
     print("3) Check-in a Table")
+    print("4) Exit program")
     print("")
 
 def menu_select(): 
     menu()
     user_input = input("Enter the number for your menu selection: ")
     print("")
-    if user_input == "q":
-        exit()
+    if user_input == "q" or user_input == "4":
+        quit_application()
     elif user_input == "1":
         view_all_tables()
         return_to_menu()
     elif user_input == "2":
-        table_select()
-        if check_if_occupied() == False:
+        selected_table_num = table_select()
+        if check_if_occupied(selected_table_num) == False:
             check_out_table(selected_table_num)
+        else:
+            print(f"Table {selected_table_num} is already checked-out")
+            print("")
         return_to_menu()
     elif user_input == "3":
-        table_select()
-        check_in_table(selected_table_num)
+        selected_table_num = table_select()
+        if check_if_occupied(selected_table_num) == True:
+            check_in_table(selected_table_num)
+            add_to_log(selected_table_num)
+        else:
+            print(f"Table {selected_table_num} is already checked-in")
+            print("")
         return_to_menu()
     else:
         print("You did not enter a valid selection. Please enter a valid selection or press \"q\" to quit")
@@ -69,9 +108,9 @@ def return_to_menu():
         print("")
         menu_select()
     elif user_input == "n":
-        exit()
+        quit_application()
     elif user_input == "q":
-        exit() 
+        quit_application()
     else:
         return_to_menu()
 
@@ -93,29 +132,33 @@ def view_all_tables():
             print (f"Number: {table.number} \nStatus: {table.is_occupied} \nStart Time: {table.start_time} \nTime Played: {time_played}\n")
 
 #select a table
+def table_select_input():
+    selected_table_input = ""
+    selected_table_num = -1
+    while selected_table_num == -1:
+        selected_table_input = input("Enter the table you would like to check-in or check-out: ")
+        if selected_table_input == "q":
+            quit_application()
+        else:
+            try:
+                selected_table_num = int(selected_table_input)
+            except ValueError:
+                print("You have not entered a valid value. Please enter a number or press \"q\" to quit.")
+    return selected_table_num
+
 def table_select():
     valid_table_nums = [i+1 for i in range(len(tables))]
-    selected_table_num = input("Enter the table you would like to check-in or check-out: ")
-    try:
-        selected_table_num = int(selected_table_num)
-        if selected_table_num not in valid_table_nums:
-            view_all_tables()
-            print("You have not entered a valid table number. Please make sure the number you select is an actual table number")
-            table_select()
-    except ValueError:
-        if selected_table_num == "q":
-            exit()
-        else:
-            view_all_tables()
-            print("You have not entered a valid value. Please enter a number.")
-            table_select()
+    selected_table_num = table_select_input()
+    while selected_table_num not in valid_table_nums:
+        view_all_tables()
+        print("You have not entered a valid table number. Please make sure the number you select is an actual table number")
+        selected_table_num = table_select_input()
+    return selected_table_num
 
 #check if occupied
-def check_if_occupied():
+def check_if_occupied(selected_table_num):
     selected_table = tables[int(selected_table_num) - 1]
     if selected_table.is_occupied == True:
-        print(f"Table {selected_table_num} is already checked-out")
-        print("")
         return True
     elif selected_table.is_occupied == False:
         return False
@@ -133,19 +176,22 @@ def check_out_table(selected_table_num):
 def check_in_table(selected_table_num):
     selected_table = tables[int(selected_table_num) - 1]
     selected_table.end_time = datetime.now()
-    selected_table.total_time = selected_table.end_time - selected_table.start_time
-    selected_table.isoccupied = False
+    selected_table.total_time = int((selected_table.end_time - selected_table.start_time).total_seconds()/60)
+    selected_table.is_occupied = False
     print(f"You have checked-in Table {selected_table_num} at {selected_table.end_time}")
     #send to file
 
+def add_to_log(selected_table_num):
+    with open(current_date_path) as file_object: 
+        table_logs = json.load(file_object)
+    selected_table = tables[int(selected_table_num) -1]
+    table_logs.append(selected_table.to_dictionary())
+    with open(current_date_path,"w") as file_object: 
+        json.dump(table_logs,file_object)
+
 tables = []
-create_initial_tables(5)
-selected_table_num = ""
-#check_out_table(selected_table_num)
-# check_in_table(selected_table_num)
-# print(tables[7].start_time, tables[7].end_time, tables[7].total_time, tables[7].is_occupied)
-#view_all_tables()
+current_date = datetime.strftime(datetime.now(), "%m-%d-%Y")
+current_date_path = f"{current_date}.json"
+load_statuses()
+create_json_log_if_needed()
 menu_select()
-
-
-##built out adding it to the receipt
