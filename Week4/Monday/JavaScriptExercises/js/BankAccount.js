2b38fe732e1826322f2360a46ba89cffa87d1982
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-15 22:37:56 -0500

    BankAccount assignment - done

--- Week4/Monday/JavaScriptExercises/js/BankAccount.js
+++ Week4/Monday/JavaScriptExercises/js/BankAccount.js
@@ -13,3 +13,44 @@
 // - User should be able to transfer money from one bank account to another  
 // - A user should be able to withdraw money from the bank account 
 // - The app should charge $-35 fees if the bank account is below $0
+
+class BankAccount {
+    constructor(firstName, lastName, accountType) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.middleName = ""
+        this.accountType = accountType
+        this.balance = 0.00
+        this.status = ""
+    }
+
+    openAccount(initialBalance) {
+        if (initialBalance >= 100) {
+            this.status = "Opened"
+            this.balance += initialBalance
+            return "Congratulations! You have opened a bank account"
+        } else {
+            return "Sorry, you must have a minimum initial balance of $100 to open an account"
+        }
+    }
+
+    transferMoney(amount, account) {
+        this.balance -= amount
+        account.balance += amount
+        if (this.balance < 0) {
+            this.balance -= 35
+        }
+        return `You have transfered $${amount} to ${account}`
+    }
+
+    withdrawMoney(amount) {
+        this.balance -= amount
+        if (this.balance < 0) {
+            this.balance -= 35
+        }
+        return `You have withdrawn $${amount}`
+    }
+}
+
+let bankAccount = new BankAccount("Kelsey","Gaetjens","Savings")
+let bankAccount2 = new BankAccount("Kelsey", "Gaetjens", "Checking")
