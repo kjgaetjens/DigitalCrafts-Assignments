3b916869f9eba37c08cfada1cfe1f0aa83b1b157
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-20 23:22:36 -0500

    TodoListjQuery - in progress - converted add new task to jQuery

--- Week4/Friday/TodoListjQuery/js/main.js
+++ Week4/Friday/TodoListjQuery/js/main.js
@@ -1,99 +1,101 @@
-let pendingTasks = document.getElementById("pendingTasks")
-let completedTasks = document.getElementById("completedTasks")
-let newTaskButton = document.getElementById("newTaskButton")
-let pendingPlaceholder = document.getElementById("pendingPlaceholder")
-let completedPlaceholder = document.getElementById("completedPlaceholder")
+$(document).ready(() => {
 
-//add pending task placeholder
-function addPendingPlaceholder() {
-    pendingPlaceholder = document.createElement("li")
-    pendingPlaceholder.id = "pendingPlaceholder"
-    pendingPlaceholder.className = "task-box placeholder"
-    let pendingPlaceholderDiv = document.createElement("div")
-    let pendingPlaceholderText = document.createElement("div")
-    pendingPlaceholderText.className = "placeholder-div"
-    pendingPlaceholderText.innerHTML = "Pending Tasks Go Here"
-    pendingPlaceholderDiv.appendChild(pendingPlaceholderText)
-    pendingPlaceholder.appendChild(pendingPlaceholderDiv)
-    pendingTasks.appendChild(pendingPlaceholder)
-}
+let $pendingTasks = $('#pendingTasks')
+let $completedTasks = $('#completedTasks')
+let $newTaskButton = $('#newTaskButton')
 
-//add completed task placeholder
-function addCompletedPlaceholder() {
-    completedPlaceholder = document.createElement("li")
-    completedPlaceholder.id = "completedPlaceholder"
-    completedPlaceholder.className = "task-box placeholder"
-    let completedPlaceholderDiv = document.createElement("div")
-    let completedPlaceholderText = document.createElement("div")
-    completedPlaceholderText.className = "placeholder-div"
-    completedPlaceholderText.innerHTML = "Completed Tasks Go Here"
-    completedPlaceholderDiv.appendChild(completedPlaceholderText)
-    completedPlaceholder.appendChild(completedPlaceholderDiv)
-    completedTasks.appendChild(completedPlaceholder)
-}
+//change ids to hyphens
 
 
-//move task to completed or back to pending
-function moveList() {
-    if(this.checked) {
-        completedTasks.appendChild(this.parentElement.parentElement)
-        if (pendingTasks.childElementCount == 0) {
-            addPendingPlaceholder()
-        }
-        completedPlaceholder.remove(this)
-    } else {
-        pendingTasks.appendChild(this.parentElement.parentElement)
-        if (completedTasks.childElementCount == 0) {
-            addCompletedPlaceholder()
-        }
-        pendingPlaceholder.remove(this)
-    }
-}
 
-//remove item
- function removeList() {
-     this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement)
-     if (pendingTasks.childElementCount == 0) {
-         addPendingPlaceholder()
-     }
-     if (completedTasks.childElementCount ==0) {
-         addCompletedPlaceholder()
-     }
- }
+// let pendingPlaceholder = document.getElementById("pendingPlaceholder")
+// let completedPlaceholder = document.getElementById("completedPlaceholder")
+
+// //add pending task placeholder
+// function addPendingPlaceholder() {
+//     pendingPlaceholder = document.createElement("li")
+//     pendingPlaceholder.id = "pendingPlaceholder"
+//     pendingPlaceholder.className = "task-box placeholder"
+//     let pendingPlaceholderDiv = document.createElement("div")
+//     let pendingPlaceholderText = document.createElement("div")
+//     pendingPlaceholderText.className = "placeholder-div"
+//     pendingPlaceholderText.innerHTML = "Pending Tasks Go Here"
+//     pendingPlaceholderDiv.appendChild(pendingPlaceholderText)
+//     pendingPlaceholder.appendChild(pendingPlaceholderDiv)
+//     pendingTasks.appendChild(pendingPlaceholder)
+// }
+
+// //add completed task placeholder
+// function addCompletedPlaceholder() {
+//     completedPlaceholder = document.createElement("li")
+//     completedPlaceholder.id = "completedPlaceholder"
+//     completedPlaceholder.className = "task-box placeholder"
+//     let completedPlaceholderDiv = document.createElement("div")
+//     let completedPlaceholderText = document.createElement("div")
+//     completedPlaceholderText.className = "placeholder-div"
+//     completedPlaceholderText.innerHTML = "Completed Tasks Go Here"
+//     completedPlaceholderDiv.appendChild(completedPlaceholderText)
+//     completedPlaceholder.appendChild(completedPlaceholderDiv)
+//     completedTasks.appendChild(completedPlaceholder)
+// }
+
+
+// //move task to completed or back to pending
+// function moveList() {
+//     if(this.checked) {
+//         completedTasks.appendChild(this.parentElement.parentElement)
+//         if (pendingTasks.childElementCount == 0) {
+//             addPendingPlaceholder()
+//         }
+//         completedPlaceholder.remove(this)
+//     } else {
+//         pendingTasks.appendChild(this.parentElement.parentElement)
+//         if (completedTasks.childElementCount == 0) {
+//             addCompletedPlaceholder()
+//         }
+//         pendingPlaceholder.remove(this)
+//     }
+// }
+
+// //remove item
+//  function removeList() {
+//      this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement)
+//      if (pendingTasks.childElementCount == 0) {
+//          addPendingPlaceholder()
+//      }
+//      if (completedTasks.childElementCount ==0) {
+//          addCompletedPlaceholder()
+//      }
+//  }
 
 //add new task to pending list
-newTaskButton.addEventListener("click", function() {
-    let newTaskInput = document.getElementById("newTask")
-    if (newTaskInput.value) {
-        let newTask = document.createElement("li")
-        newTask.className = "task-box"
+//prob get rid of the if statement if able to 
+$newTaskButton.on('click', function() {
+    let $newTaskInput = $('#newTask')
+    if ($newTaskInput.val()) {
+        let $newTask = $(`
+        <li class="task-box">
+            <div class="check-box-div">
+                <input type="checkbox">
+            </div>
+            <div class="title-div">
+                <h3>${$newTaskInput.val()}</h3>
+            </div>
+            <div class="remove-div">
+                <a>Remove</a>
+            </div>
+        </li>
+        `)
     
-        let taskCheckBoxDiv = document.createElement("div")
-        taskCheckBoxDiv.className = "check-box-div"
-        let taskCheckBox = document.createElement("input")
-        taskCheckBox.type = "checkbox"
-        taskCheckBox.addEventListener("click", moveList)
-        taskCheckBoxDiv.appendChild(taskCheckBox)
-    
-        let taskTitleDiv = document.createElement("div")
-        taskTitleDiv.className = "title-div"
-        let taskTitle = document.createElement("h3")
-        taskTitle.innerHTML = newTaskInput.value
-        taskTitleDiv.appendChild(taskTitle)
-    
-        let taskRemoveDiv = document.createElement("div")
-        taskRemoveDiv.className = "remove-div"
-        let taskRemove = document.createElement("a")
-        taskRemove.innerHTML = "Remove"
-        taskRemove.addEventListener("click", removeList)
-        taskRemoveDiv.appendChild(taskRemove)
-    
-        newTask.appendChild(taskCheckBoxDiv)
-        newTask.appendChild(taskTitleDiv)
-        newTask.appendChild(taskRemoveDiv)
-    
-        pendingPlaceholder.remove(this)
-        pendingTasks.appendChild(newTask)
-        newTaskInput.value=""
+        // taskCheckBox.addEventListener("click", moveList)
+        // taskRemove.addEventListener("click", removeList)
+        // pendingPlaceholder.remove(this)
+
+        $newTask.appendTo($pendingTasks)
+        $newTaskInput.val('')
     }
 })
+
+
+})
+
