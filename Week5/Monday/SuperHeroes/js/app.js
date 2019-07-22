39a8e17aed4096cebc09f8c24a7d57a4693f1744
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-22 13:31:42 -0500

    SuperHeroes assignment - in progress - generated initial array

--- Week5/Monday/SuperHeroes/js/app.js
+++ Week5/Monday/SuperHeroes/js/app.js
@@ -12,3 +12,36 @@
 // Feel free to use your design skills to create an amazing experience. 
 // * Don't forget to make your website responsive
 // * You can also make your page take movie name input and then send it as part of the request. 
+
+
+
+let batmanURL = "http://www.omdbapi.com/?s=batman&apikey=91f2fc7"
+let movieList = document.getElementById('movie-list')
+
+let req = new XMLHttpRequest()
+req.open('GET', batmanURL)
+
+//pull in object array
+req.addEventListener('load', () => {
+    let moviesObject = JSON.parse(event.currentTarget.responseText)
+    let movies = moviesObject.Search
+    console.log(movies)
+    let movieLIs = movies.map(movie => {
+        return `<li class="movie">
+                    <div>
+                        <img class="movie-poster" src="${movie.Poster}"/>
+                    </div>
+                    <div>
+                        <a class="movie-link" href="http://www.omdbapi.com/?i=${movie.imdbID}&apikey=91f2fc7">${movie.Title}</a>
+                    </div>
+                </li>
+                `
+    })
+    movieList.innerHTML = movieLIs.join('')
+})
+
+
+
+
+
+req.send()
