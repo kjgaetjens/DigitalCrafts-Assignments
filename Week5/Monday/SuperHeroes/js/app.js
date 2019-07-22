2f08c87837d58b6a8c2433f166bbffd36ab470ee
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-22 15:04:11 -0500

    SuperHeroes assignment - done

--- Week5/Monday/SuperHeroes/js/app.js
+++ Week5/Monday/SuperHeroes/js/app.js
@@ -17,22 +17,49 @@
 
 let batmanURL = "http://www.omdbapi.com/?s=batman&apikey=91f2fc7"
 let movieList = document.getElementById('movie-list')
+let detailList = document.getElementById('movie-details')
 
 let req = new XMLHttpRequest()
 req.open('GET', batmanURL)
+
+const displayDetails = url => {
+    let detailURL = url
+    let detailreq = new XMLHttpRequest()
+    detailreq.open('GET', detailURL)
+
+    detailreq.addEventListener('load', () => {
+        let details = JSON.parse(event.currentTarget.responseText)
+        let detailLI = `<li class="movie">
+            <div>
+                <h2>${details.Title}</h2>
+            </div>
+            <div>
+                <img class="movie-poster" src="${details.Poster}"/>
+            </div>
+            <div>
+                <h3>Year: ${details.Year}</h3>
+                <h3>Rated: ${details.Rated}</h3>
+                <h3>Released: ${details.Released}</h3>
+                <h3>Director: ${details.Director}</h3>
+            </div>
+        </li>
+        `
+        detailList.innerHTML = detailLI
+    })
+    detailreq.send()
+}
 
 //pull in object array
 req.addEventListener('load', () => {
     let moviesObject = JSON.parse(event.currentTarget.responseText)
     let movies = moviesObject.Search
-    console.log(movies)
     let movieLIs = movies.map(movie => {
         return `<li class="movie">
                     <div>
                         <img class="movie-poster" src="${movie.Poster}"/>
                     </div>
                     <div>
-                        <a class="movie-link" href="http://www.omdbapi.com/?i=${movie.imdbID}&apikey=91f2fc7">${movie.Title}</a>
+                        <a class="movie-link" href=#movie-details onclick="displayDetails('http://www.omdbapi.com/?i=${movie.imdbID}&apikey=91f2fc7')">${movie.Title}</a>
                     </div>
                 </li>
                 `
