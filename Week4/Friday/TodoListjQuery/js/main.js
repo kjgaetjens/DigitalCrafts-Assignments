8cab2617fd3b965888277e957bdba733bca6de9c
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-23 13:30:04 -0500

    TodoListjQuery assignment - done

--- Week4/Friday/TodoListjQuery/js/main.js
+++ Week4/Friday/TodoListjQuery/js/main.js
@@ -7,64 +7,8 @@ let $completedTasks = $('#completedTasks')
 let $newTaskButton = $('#newTaskButton')
 
 let $pendingPlaceholder = $('#pendingPlaceholder')
-let $completedPlaceholder = $('#completedPlaceholder')
+let $completedPlaceholder = $('#completedPlaceholder') 
 
-// //add pending task placeholder
-// function addPendingPlaceholder() {
-//     pendingPlaceholder = document.createElement("li")
-//     pendingPlaceholder.id = "pendingPlaceholder"
-//     pendingPlaceholder.className = "task-box placeholder"
-//     let pendingPlaceholderDiv = document.createElement("div")
-//     let pendingPlaceholderText = document.createElement("div")
-//     pendingPlaceholderText.className = "placeholder-div"
-//     pendingPlaceholderText.innerHTML = "Pending Tasks Go Here"
-//     pendingPlaceholderDiv.appendChild(pendingPlaceholderText)
-//     pendingPlaceholder.appendChild(pendingPlaceholderDiv)
-//     pendingTasks.appendChild(pendingPlaceholder)
-// }
-
-// //add completed task placeholder
-// function addCompletedPlaceholder() {
-//     completedPlaceholder = document.createElement("li")
-//     completedPlaceholder.id = "completedPlaceholder"
-//     completedPlaceholder.className = "task-box placeholder"
-//     let completedPlaceholderDiv = document.createElement("div")
-//     let completedPlaceholderText = document.createElement("div")
-//     completedPlaceholderText.className = "placeholder-div"
-//     completedPlaceholderText.innerHTML = "Completed Tasks Go Here"
-//     completedPlaceholderDiv.appendChild(completedPlaceholderText)
-//     completedPlaceholder.appendChild(completedPlaceholderDiv)
-//     completedTasks.appendChild(completedPlaceholder)
-// }
-
-
-// //move task to completed or back to pending
-// function moveList() {
-//     if(this.checked) {
-//         completedTasks.appendChild(this.parentElement.parentElement)
-//         if (pendingTasks.childElementCount == 0) {
-//             addPendingPlaceholder()
-//         }
-//         completedPlaceholder.remove(this)
-//     } else {
-//         pendingTasks.appendChild(this.parentElement.parentElement)
-//         if (completedTasks.childElementCount == 0) {
-//             addCompletedPlaceholder()
-//         }
-//         pendingPlaceholder.remove(this)
-//     }
-// }
-
-// //remove item
-//  function removeList() {
-//      this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement)
-//      if (pendingTasks.childElementCount == 0) {
-//          addPendingPlaceholder()
-//      }
-//      if (completedTasks.childElementCount ==0) {
-//          addCompletedPlaceholder()
-//      }
-//  }
 
 //add new task to pending list
 //prob get rid of the if statement if able to 
@@ -74,25 +18,52 @@ $newTaskButton.on('click', function() {
         let $newTask = $(`
         <li class="task-box">
             <div class="check-box-div">
-                <input type="checkbox">
+                <input class="task-complete" type="checkbox">
             </div>
             <div class="title-div">
                 <h3>${$newTaskInput.val()}</h3>
             </div>
             <div class="remove-div">
-                <a>Remove</a>
+                <a class="task-remove">Remove</a>
             </div>
         </li>
         `)
     
-        // taskCheckBox.addEventListener("click", moveList)
-        // taskRemove.addEventListener("click", removeList)
         $pendingPlaceholder.hide()
         $newTask.appendTo($pendingTasks)
         $newTaskInput.val('')
     }
 })
 
+//move task to completed or back to pending
+//make sure it never shows 2 placeholders in same list
+$(document).on('click', '.task-complete', event => {
+    if ($(event.currentTarget).prop("checked")) {
+        $(event.currentTarget).closest('li').appendTo($completedTasks)
+        if ($pendingTasks.children().length == 1) {
+            $pendingPlaceholder.show()
+        }
+        $completedPlaceholder.hide()
+    } else {
+        $(event.currentTarget).closest('li').appendTo($pendingTasks)
+        if ($completedTasks.children().length == 1) {
+            $completedPlaceholder.show()
+        }
+        $pendingPlaceholder.hide()
+    }
+})
+
+ //remove
+ $(document).on('click', '.task-remove', event => {
+    $(event.currentTarget).closest('li').remove()
+    if ($pendingTasks.children().length == 1) {
+        console.log('test')
+        $pendingPlaceholder.show()
+    }
+    if ($completedTasks.children().length == 1) {
+        $completedPlaceholder.show()
+    }
+ })
 
 })
 
