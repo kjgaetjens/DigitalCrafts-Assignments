519e1769fb01dc2afbba573f6840c4e7adbf04eb
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-24 13:54:39 -0500

    GroceryAppFirebase assignment - in progress - set up framework

---
+++ Week5/Wednesday/GroceryAppFirebase/js/app.js
@@ -0,0 +1,20 @@
+// Create a grocery list app which allows you to add different categories. Here are list of requested features. 
+// * User should be able to add the grocery category (Walmart, HEB, Fiesta, Sams Club etc)  (name and address)
+// * User should be able to view all grocery categories 
+// * User should be able to delete grocery categories
+// * Items must be stored in Firebase database 
+// * The website should be mobile responsive 
+
+// HARD MODE:
+// * Add user registration and authentication 
+// * Allow user to add items to the grocery categories
+// * Allow the user to login and then store the categories for that user. This means each category in Firebase will be associated with a particular user. 
+
+let groceryCatRef = database.ref('categories')
+
+
+
+
+
+// groceryCatRef.on('value', snap => console.log(snap.val()))
+
