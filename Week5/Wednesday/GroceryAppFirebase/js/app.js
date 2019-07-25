438bd36000fa620e3ce709a9b7f21eb6e3234c1d
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-07-24 22:37:11 -0500

    GroceryAppFirebase assignment - done

--- Week5/Wednesday/GroceryAppFirebase/js/app.js
+++ Week5/Wednesday/GroceryAppFirebase/js/app.js
@@ -10,11 +10,59 @@
 // * Allow user to add items to the grocery categories
 // * Allow the user to login and then store the categories for that user. This means each category in Firebase will be associated with a particular user. 
 
+
+
+//declare variables
 let groceryCatRef = database.ref('categories')
+let catList = document.getElementById('cat-list')
+let addCatButton = document.getElementById('add-cat-button')
+let catNameField = document.getElementById('cat-name')
+let catAddressField = document.getElementById('cat-address')
 
+//add data (make sure to add if/else validation to make sure they entered info into cat name field)
+addCatButton.addEventListener('click', () => {
+    catName = catNameField.value
+    catAddress = catAddressField.value
+    addCat(catName, catAddress)
+    catNameField.value = ''
+    catAddressField.value = ''
+})
 
+const addCat = (name, address) => {
+    groceryCatRef.push({
+        name: name,
+        address: address
+    })
+}
 
+const deleteCat = (id) => {
+    groceryCatRef.child(id).remove()
+}
 
+groceryCatRef.on('value', snap => {
+    let catValuesArray = []
 
-// groceryCatRef.on('value', snap => console.log(snap.val()))
+    for(key in snap.val()) {
+        let cat = snap.val()[key]
+        cat.id = key
+        catValuesArray.push(cat)
+    }
 
+    displayCat(catValuesArray)
+})
+
+const displayCat = cats => {
+    let catDivArray = cats.map(cat => {
+        return `
+                <li class="cat-li">
+                    <div class="cat-container">
+                        <span class="cat-name-header">${cat.name}: <span>
+                        <span class="cat-address-header">${cat.address}</span>
+                        <br>
+                        <button onclick="deleteCat('${cat.id}')">Delete</button>
+                    <div>
+                </li>
+                `
+    })
+    catList.innerHTML = catDivArray.join('')
+}
