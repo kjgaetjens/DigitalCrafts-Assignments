669343b46556c1549193102520d17e6c47686141
Kelsey Gaetjens <kjgaetjens@gmail.com>
2019-08-08 22:39:57 -0500

    TripsAuth assignment - done - updated instructions comment

--- Week7/Thursday/TripsAuth/app.js
+++ Week7/Thursday/TripsAuth/app.js
@@ -1,17 +1,15 @@
 /*
 You are in charge of creating a website for tracking trips. You will use server side pages using Mustache or any other server side template framework for this assignment. 
-
 Your app should allow users to do the following: 
 - Ability to add a new trip. A new trip consists of title, image, date of departure, date of return 
-- Ability to view all the trips (A sample screenshot is shown below. Screenshot is just for demoeing purposes you can design your app however you want) 
+- Ability to view all the trips (A sample screenshot is shown below) 
 - Ability to delete a trip 
+- Add the ability to allow user to register for the website 
+- Allow the user to login to the website 
+- Allow the user to signout from the website 
+- Allow the user to only see their trips after they login successfully
 * Your app should work on mobile devices 
 * Google Trips is a good app for inspiration and ideas! (Available on the App Store)  
-
-HARDMODE: 
-- Ability to update the trip 
-- Ability to sort the trip by date
-- Ability to search for the trip by destination city
 */
 
 
